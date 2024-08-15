1f2693f6abb56137e882cdb4e7dcb1f7fb19b124
tim-rayner <tim.rayner2020@gmail.com>
2024-08-15 17:58:06 +0100

    feat: added 'add plant' form

---
+++ app/new.tsx
@@ -0,0 +1,97 @@
+import { Text, StyleSheet, TextInput, Alert, View } from "react-native";
+
+import { theme } from "@/theme";
+import { PlantlyButton } from "@/components/PlantlyButton";
+import { useState } from "react";
+import { PlantlyImage } from "@/components/PlantlyImage";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+
+import { usePlantStore } from "@/store/plantsStore";
+import { useRouter } from "expo-router";
+
+export default function NewScreen() {
+  const router = useRouter();
+  const addPlant = usePlantStore((state) => state.addPlant);
+
+  const [name, setName] = useState<string>();
+  const [days, setDays] = useState<string>();
+
+  const handleSubmit = () => {
+    if (!name) {
+      return Alert.alert("Validation Error", "Give your plant a name");
+    }
+
+    if (!days) {
+      return Alert.alert(
+        "Validation Error",
+        `How often does ${name} need to be watered?`,
+      );
+    }
+
+    if (Number.isNaN(Number(days))) {
+      return Alert.alert(
+        "Validation Error",
+        "Watering frequency must be a be a number",
+      );
+    }
+
+    addPlant(name, Number(days));
+    router.navigate("/");
+  };
+
+  return (
+    <KeyboardAwareScrollView
+      style={styles.container}
+      contentContainerStyle={styles.contentContainer}
+      keyboardShouldPersistTaps="handled"
+    >
+      <View style={styles.centered}>
+        <PlantlyImage />
+      </View>
+      <Text style={styles.label}>Name</Text>
+      <TextInput
+        value={name}
+        onChangeText={setName}
+        style={styles.input}
+        placeholder="E.g. Casper the Cactus"
+        autoCapitalize="words"
+      />
+      <Text style={styles.label}>Watering Frequency (every x days)</Text>
+      <TextInput
+        value={days}
+        onChangeText={setDays}
+        style={styles.input}
+        placeholder="E.g. 6"
+        keyboardType="number-pad"
+      />
+      <PlantlyButton title="Add plant" onPress={handleSubmit} />
+    </KeyboardAwareScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.colorWhite,
+  },
+  contentContainer: {
+    paddingTop: 24,
+    paddingHorizontal: 24,
+    paddingBottom: 100,
+  },
+  input: {
+    borderWidth: 2,
+    borderColor: theme.colorLightGrey,
+    padding: 12,
+    borderRadius: 6,
+    marginBottom: 24,
+    fontSize: 18,
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 8,
+  },
+  centered: {
+    alignItems: "center",
+  },
+});
--- theme.ts
+++ theme.ts
@@ -4,4 +4,7 @@ export const theme = {
   colorLeafyGreen: "#206a42",
   colorAppleGreen: "#a0d36c",
   colorLimeGreen: "#d0e57e",
+  colorLightGrey: "#eee",
+  colorBlack: "#000",
+  colorGrey: "#808080",
 };
