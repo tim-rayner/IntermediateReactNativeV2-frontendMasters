66c6622769f0995660c62f32490a0e0b08c00c27
tim-rayner <tim.rayner2020@gmail.com>
2024-08-15 18:29:22 +0100

    feat: added plant details screen showing plant and watering status details

--- components/PlantCard.tsx
+++ components/PlantCard.tsx
@@ -1,21 +1,25 @@
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Pressable } from "react-native";
+import { Link } from "expo-router";
+
 import { theme } from "@/theme";
 import { PlantType } from "@/store/plantsStore";
 import { PlantlyImage } from "./PlantlyImage";
 
 export function PlantCard({ plant }: { plant: PlantType }) {
   return (
-    <View style={styles.plantCard}>
-      <PlantlyImage size={100} imageUri={plant.imageUri} />
-      <View style={styles.details}>
-        <Text numberOfLines={1} style={styles.plantName}>
-          {plant.name}
-        </Text>
-        <Text style={styles.subtitle}>
-          Water every {plant.wateringFrequencyDays} days
-        </Text>
-      </View>
-    </View>
+    <Link href={`plants/${plant.id}`} asChild>
+      <Pressable style={styles.plantCard}>
+        <PlantlyImage size={100} imageUri={plant.imageUri} />
+        <View style={styles.details}>
+          <Text numberOfLines={1} style={styles.plantName}>
+            {plant.name}
+          </Text>
+          <Text style={styles.subtitle}>
+            Water every {plant.wateringFrequencyDays} days
+          </Text>
+        </View>
+      </Pressable>
+    </Link>
   );
 }
 
